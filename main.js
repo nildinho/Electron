8b2ea46398441beb1e891ab1f21dffb3973e92a5
nildinho <nildinho.anahi@hotmail.com>
2024-06-10 13:50:50 -0300

    pwa

--- main.js
+++ main.js
@@ -1,16 +1,95 @@
 //console.log("Processo Principal")
 
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, nativeTheme, Menu, shell } = require('electron')
 
+// Janela Principal
 const createWindow = () => {
+  // nativeTheme.themeSource = 'dark'
   const win = new BrowserWindow({
-    width: 800,
-    height: 600
+    width: 800, //largura
+    height: 600, //altura
+    icon: './src/public/img/pc.png'
+    //resizable: false, //evitar o redimensionamento
+    //titleBarStyle:'hidden', //esconder barra de título e menu
+    //autoHideMenuBar: true //esconder o menu
   })
 
-  //win.loadFile('index.html')
+  // iniciar a janela com o menu personalizado
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+
+  win.loadFile('./src/views/index.html')
 }
 
+// Janela sobre
+const aboutWindow = () => {
+  const about = new BrowserWindow({
+    width: 360, //largura
+    height: 220, //altura
+    icon: './src/public/img/pc.png',
+    resizable: false, //evitar o redimensionamento
+    autoHideMenuBar: true //esconder o menu
+  })
+
+  about.loadFile('./src/views/sobre.html')
+}
+
+// executar de forma assincrona a aplicação
 app.whenReady().then(() => {
   createWindow()
 })
+
+// template do menu personalizado
+
+const template = [
+  {
+    label: 'Arquivo',
+    submenu: [
+      {
+        label: 'Sair',
+        click: () => app.quit(),
+        accelerator: 'Alt+F4'
+      }
+    ]
+
+  },
+  {
+    label: 'Exibir',
+    submenu: [
+      {
+        label:'Recarregar',
+        role:'reload'
+    },
+    {
+        label: 'Ferramentas do desenvolvedor',
+        role:'toggleDevTools'
+    },
+    {
+        label:'Aplicar zoom',
+        role: 'zoomIn'
+    },
+    {
+      label: 'Reduzir',
+      role: 'zoomOut'
+    }
+  ]
+  },
+  {
+    label: 'Ajuda',
+    submenu: [
+      {
+        label: 'docs',
+        click: () => shell.openExternal('https://www.electronjs.org/docs/latest/')
+      },
+      {
+        type: 'separator'
+      },
+      {
+        label: 'sobre',
+        accelerator: 'Alt=F1',
+        click: () => aboutWindow()
+      }
+        
+
+    ]
+  }
+]
